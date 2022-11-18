f0c796236bfcc446da22a0135f92a8e33ffc657c
MatteoRobbiati <robbiati.matteo@gmail.com>
2022-11-18 14:55:09 +0100

    Revert "new test with jupyter notebook rendering"
    This reverts commit 256c1404324a479388eacc95ce1b6a1c011ff98f.

--- components/Notebook.js
+++
@@ -1,9 +0,0 @@
-import { JupyterNotebookViewer } from "react-jupyter-notebook-viewer";
-
-function Notebook(props) {
-  const notebook = new JupyterNotebookViewer(props);
-
-  return <>{notebook}</>;
-}
-
-export default Notebook;
